8d2e844965e04e9201ccd03816c97686876d3d17
Maarten <r0761272@student.thomasmore.be>
2019-12-24 16:25:21 +0100

    First commit

---
+++ Assets/Scripts/AdventureGame.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AdventureGame : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField] Text textComponent;
+    [SerializeField] State startingState;
+    State state;
+    bool roomKey;
+
+
+    void Start()
+    {
+        state = startingState;
+        textComponent.text = state.getStateStory();
+        roomKey = false;
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        ManageState();
+        if (state.getRoomKey())
+        {
+            roomKey = state.getRoomKey();
+        }
+    }
+
+    private void ManageState()
+    {
+        var nextStates = state.getNextstates();
+        if (state.getRoomDoor())
+        {
+            if (Input.GetKeyDown(KeyCode.Alpha1))
+            {
+                if (roomKey)
+                {
+                    state = nextStates[2];
+                }
+                else
+                {
+                    state = nextStates[0];
+                }
+                
+            }else if (Input.GetKeyDown(KeyCode.Alpha2))
+            {
+                state = nextStates[1];
+            }
+        }
+        else
+        {
+            for (int i = 0; i < nextStates.Length; i++)
+            {
+                if (Input.GetKeyDown(KeyCode.Alpha1 + i))
+                {
+                    state = nextStates[i];
+                }
+            }
+        }
+
+        textComponent.text = state.getStateStory();
+    }
+}
---
+++ Assets/Scripts/State.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "State")]
+public class State : ScriptableObject
+{
+    
+    [TextArea(14,10)] [SerializeField] string storyText;
+    [SerializeField] State[] nextStates;
+    [SerializeField] bool roomKey;
+    [SerializeField] bool roomDoor;
+
+    public State[] getNextstates()
+    {
+ 
+        return nextStates;
+    }
+
+    public string getStateStory()
+    {
+        return storyText;
+    }
+
+    public bool getRoomKey()
+    {
+        return roomKey;
+    }
+
+    public bool getRoomDoor()
+    {
+        return roomDoor;
+    }
+}
